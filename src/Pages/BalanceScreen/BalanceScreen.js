702b3e7b43fa2dcc68ab0129efc1986b882b8f2c
Kamil <kamilpier08@gmail.com>
2018-09-05 18:33:06 +0200

    charts

--- src/Pages/BalanceScreen/BalanceScreen.js
+++ src/Pages/BalanceScreen/BalanceScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, FlatList, Dimensions } from 'react-native';
 import { Header } from '../../Components/Header';
 import firebase from 'react-native-firebase';
 import { MySpinner } from '../../Components/Spinner';
+import { LineChart } from 'react-native-chart-kit';
 
 export class BalanceScreen extends Component {
   constructor(props){
@@ -26,19 +27,17 @@ export class BalanceScreen extends Component {
     this.accounts.onSnapshot(doc => {
       doc.forEach( account => {
         this.payments.where('accountId','==',account.id).where('deleted','==',false).onSnapshot( payments => {
-          let saldo = 0;
           let paymentsArray = [];
           payments.forEach( payment => {
             payment = payment.data();
             if(payment.kind == 'creditor'){
               paymentsArray.push(payment.amount);
-              saldo += payment.amount;
             } else {
               paymentsArray.push(-payment.amount);
-              saldo -= payment.amount;
             }
           })
-          this.pushAccount({username: account.data().username,id: account.id,key: account.id, saldo : saldo, payments: paymentsArray});
+          if(paymentsArray.length > 1)
+            this.pushAccount({username: account.data().username,id: account.id,key: account.id, payments: paymentsArray});
         })        
       });      
     })
@@ -57,37 +56,47 @@ export class BalanceScreen extends Component {
     })
     if (!exists) accounts.push(newAccount);
     this.setState({accounts});
+
   }
 
   render() {
     return (
       <View style={styles.Container}>
           <Header title="Bilans" />
-          {/* <LineChart
-                data={{
-                  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-                  datasets: [{
-                    data: [
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100
-                    ]
-                  }]
-                }}
-                width={Dimensions.get('window').width} // from react-native
-                height={220}
-                chartConfig={{
-                  backgroundColor: '#e26a00',
-                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                }}
-                bezier
-                style={{
-                  marginVertical: 10,
-                }}
-              /> */}
+          <FlatList 
+            data={this.state.accounts}
+            renderItem={({item}) => (
+
+              <View style={styles.chartBox}>
+                <Text style={styles.username}>{item.username}</Text>
+                <LineChart
+                  data={{
+                    datasets: [{
+                      data: item.payments
+                    }]
+                  }}
+                  width={Dimensions.get('window').width - 20} // from react-native
+                  height={220}
+                  chartConfig={{
+                    backgroundColor: '#e26a00',
+                    backgroundGradientFrom: '#fb8c00',
+                    backgroundGradientTo: '#ffa726',
+                    color: (opacity = 1) => `rgba(50, 50, 50, ${opacity})`,
+                    style: {
+                      borderRadius: 16
+                    }
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                    elevation: 1,
+                  }}
+                />
+              </View>
+
+            )} 
+          />      
 
           {/* <BalanceUserItem username="Mama" amount="-100,24" details={[-50,-20,-30,80,125,684]}/>
           <BalanceUserItem username="Mama" amount="-100,24" details={[-50,-20,684]}/>
@@ -103,5 +112,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: '100%',
   },
+  chartBox: {
+    marginBottom: 15,
+    marginHorizontal: 10,
+  },
+  username: {
+    textAlign: 'center',
+    fontSize: 18,
+  }
 })
 
